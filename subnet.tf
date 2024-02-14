5c66c27dd4e38b9f96d6f08b8a9002b93304858e
23-Pravallika <learngain23@gmail.com>
2024-02-14 17:00:22 +0530

    AutoPush

---
+++ subnet.tf
@@ -0,0 +1,24 @@
+# Creates Public Subnets
+resource "aws_subnet" "public_subnet" {
+  count = length(var.PUBLIC_SUBNET)  
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PUBLIC_SUBNET, count.index)
+  availability_zone = element(var.AZ, count.index)
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "robo-public-subnet-${element(var.AZ, count.index)}"
+  }
+}
+
+# Creates Private Subnets
+resource "aws_subnet" "private_subnet" {
+  count = length(var.PRIVATE_SUBNET)  
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PRIVATE_SUBNET, count.index)
+  availability_zone = element(var.AZ, count.index)
+
+  tags = {
+    Name = "robo-private-subnet-${element(var.AZ, count.index)}"
+  }
+}
